c610e6b83e47941be94fa1ef3240fe04ab612f5b
Ernest <musaubrian15@gmail.com>
2023-03-12 21:05:47 +0300

    chore(api): update Post model

---
+++ api/prisma/migrations/20230312175837_post_update/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `title` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "content" TEXT,
+ADD COLUMN     "title" TEXT NOT NULL;
