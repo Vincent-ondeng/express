3e721852d467f8e0fefbcca215afa0ceb46d3064
Ernest <musaubrian15@gmail.com>
2023-03-15 22:07:27 +0300

    chore: restructure src/ dir

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,78 +1,10 @@
-import express from "express";
-import { resolve } from "path";
-import userAuth from "../db/auth";
-import posts from "../db/post";
-import user from "../db/user";
+import express from 'express';
+import routes from './routes/index.routes';
 const PORT = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
-
-app.post("/users/new", async (req, res) => {
-  const { email, username, password } = req.body;
-  const results = await user.new(email, username, password);
-  res.status(201).json(results);
-});
-
-app.post("/users/login", async (req, res) => {
-  const { email, password } = req.body;
-  const result = await userAuth.login(email, password);
-
-  if (result) {
-    res.status(200).json(result);
-  } else {
-    res.status(403).json({ error: "email or password is incorrect" });
-  }
-});
-
-app.get("/users/:id", async (req, res) => {
-  const { id } = req.params;
-
-  const results = await user.single(parseInt(id));
-  res.status(200).json(results);
-});
-
-app.get("/users/:id/posts", async (req, res) => {
-  const { id } = req.params;
-  const results = await posts.singleUser(parseInt(id));
-
-  res.json(results);
-});
-app.get("/users/:id/posts/:postID", async (req, res) => {
-  const { id, postID } = req.params;
-  const results = await posts.single(parseInt(postID));
-  res.status(200).json(results);
-});
-
-app.post("/users/:id/posts/new", async (req, res) => {
-  const { id } = req.params;
-  const { title, content } = req.body;
-  const results = await posts.new(parseInt(id), title, content);
-  res.status(201).json(results);
-});
-
-app.delete("/users/:id/posts/:postID", async (req, res) => {
-  const { id, postID } = req.params;
-  await posts.delete(parseInt(postID));
-  res.status(204).json({ message: "successfully deleted post" });
-});
-
-app.get("/users/:id/posts/drafts", async (req, res) => {
-  const { id } = req.params;
-  const results = await posts.drafts(parseInt(id));
-  res.json(results);
-});
-
-app.get("/feed", async (req, res) => {
-  const result = await posts.all();
-  res.json(result);
-});
-
-app.get("/feed/:id", async (req, res) => {
-  const { id } = req.params;
-  const results = await posts.single(parseInt(id));
-  res.json(results);
-});
+app.use("/", routes);
 
 app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
---
+++ api/src/routes/index.routes.ts
@@ -0,0 +1,74 @@
+import { Router } from 'express';
+import userAuth from '../../db/auth';
+import posts from '../../db/post';
+import user from '../../db/user';
+
+const router = Router();
+
+router.post('/users/new', async (req, res) => {
+  const { email, username, password } = req.body;
+  const results = await user.new(email, username, password);
+  res.status(201).json(results);
+});
+
+router.post('/users/login', async (req, res) => {
+  const { email, password } = req.body;
+  const result = await userAuth.login(email, password);
+
+  if (result) {
+    res.status(200).json(result);
+  } else {
+    res.status(403).json({ error: 'email or password is incorrect' });
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const results = await user.single(parseInt(id));
+  res.status(200).json(results);
+});
+
+router.get('/users/:id/posts', async (req, res) => {
+  const { id } = req.params;
+  const results = await posts.singleUser(parseInt(id));
+
+  res.json(results);
+});
+router.get('/users/:id/posts/:postID', async (req, res) => {
+  const { id, postID } = req.params;
+  const results = await posts.single(parseInt(postID));
+  res.status(200).json(results);
+});
+
+router.post('/users/:id/posts/new', async (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+  const results = await posts.new(parseInt(id), title, content);
+  res.status(201).json(results);
+});
+
+router.delete('/users/:id/posts/:postID', async (req, res) => {
+  const { id, postID } = req.params;
+  await posts.delete(parseInt(postID));
+  res.status(204).json({ message: 'successfully deleted post' });
+});
+
+router.get('/users/:id/posts/drafts', async (req, res) => {
+  const { id } = req.params;
+  const results = await posts.drafts(parseInt(id));
+  res.json(results);
+});
+
+router.get('/feed', async (req, res) => {
+  const result = await posts.all();
+  res.json(result);
+});
+
+router.get('/feed/:id', async (req, res) => {
+  const { id } = req.params;
+  const results = await posts.single(parseInt(id));
+  res.json(results);
+});
+
+export default router;
