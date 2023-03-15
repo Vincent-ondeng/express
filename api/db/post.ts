639a54a777b18844396854c30b9c8cadb67e9537
Ernest <musaubrian15@gmail.com>
2023-03-15 13:44:08 +0300

    chore: add comments

--- api/db/post.ts
+++ api/db/post.ts
@@ -3,30 +3,36 @@ import { PrismaClient } from ".prisma/client";
 const prisma = new PrismaClient();
 
 class PostFunctions {
+  // Get all the (published) posts
   async all() {
     return await prisma.post.findMany({
       where: { published: true },
     });
   }
 
+  // Create a new post linked to the userID
   async new(userID: number, postTitle: string, postContent: string) {
     return await prisma.post.create({
       data: { title: postTitle, content: postContent, authorId: userID },
     });
   }
 
+  // Get posts of a single user
+  // Can be used when showing a users profile?
   async singleUser(userID: number) {
     return await prisma.post.findMany({
       where: { authorId: userID },
     });
   }
 
+  // Show drafts related to a user
   async drafts(userID: number) {
     return await prisma.post.findMany({
       where: { published: false, authorId: userID },
     });
   }
 
+  // Get a single post
   async single(postID: number) {
     return await prisma.post.findUnique({
       where: { id: postID },
