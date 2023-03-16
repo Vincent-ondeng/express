3c97765e4119691fc02a08156d4b6b537d0b4dbb
Ernest <musaubrian15@gmail.com>
2023-03-16 07:55:54 +0300

    chore: format index.routes.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import routes from './routes/index.routes';
+
 const PORT = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
-app.use("/", routes);
+app.use('/', routes);
 
 app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
--- api/src/routes/index.routes.ts
+++ api/src/routes/index.routes.ts
@@ -3,15 +3,15 @@ import userAuth from '../../db/auth';
 import posts from '../../db/post';
 import user from '../../db/user';
 
-const router = Router();
+const routes = Router();
 
-router.post('/users/new', async (req, res) => {
+routes.post('/users/new', async (req, res) => {
   const { email, username, password } = req.body;
   const results = await user.new(email, username, password);
   res.status(201).json(results);
 });
 
-router.post('/users/login', async (req, res) => {
+routes.post('/users/login', async (req, res) => {
   const { email, password } = req.body;
   const result = await userAuth.login(email, password);
 
@@ -22,53 +22,53 @@ router.post('/users/login', async (req, res) => {
   }
 });
 
-router.get('/users/:id', async (req, res) => {
+routes.get('/users/:id', async (req, res) => {
   const { id } = req.params;
 
   const results = await user.single(parseInt(id));
   res.status(200).json(results);
 });
 
-router.get('/users/:id/posts', async (req, res) => {
+routes.get('/users/:id/posts/live', async (req, res) => {
   const { id } = req.params;
   const results = await posts.singleUser(parseInt(id));
 
   res.json(results);
 });
-router.get('/users/:id/posts/:postID', async (req, res) => {
+routes.get('/users/:id/posts/live/:postID', async (req, res) => {
   const { id, postID } = req.params;
   const results = await posts.single(parseInt(postID));
   res.status(200).json(results);
 });
 
-router.post('/users/:id/posts/new', async (req, res) => {
+routes.post('/users/:id/posts/new', async (req, res) => {
   const { id } = req.params;
   const { title, content } = req.body;
   const results = await posts.new(parseInt(id), title, content);
   res.status(201).json(results);
 });
 
-router.delete('/users/:id/posts/:postID', async (req, res) => {
+routes.delete('/users/:id/posts/live/:postID', async (req, res) => {
   const { id, postID } = req.params;
   await posts.delete(parseInt(postID));
   res.status(204).json({ message: 'successfully deleted post' });
 });
 
-router.get('/users/:id/posts/drafts', async (req, res) => {
+routes.get('/users/:id/posts/drafts', async (req, res) => {
   const { id } = req.params;
   const results = await posts.drafts(parseInt(id));
   res.json(results);
 });
 
-router.get('/feed', async (req, res) => {
+routes.get('/feed', async (req, res) => {
   const result = await posts.all();
   res.json(result);
 });
 
-router.get('/feed/:id', async (req, res) => {
+routes.get('/feed/:id', async (req, res) => {
   const { id } = req.params;
   const results = await posts.single(parseInt(id));
   res.json(results);
 });
 
-export default router;
+export default routes;
