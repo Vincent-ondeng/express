20cccfd88b5c83f559b882534349aa19d56ebb3f
Ernest <musaubrian15@gmail.com>
2023-03-22 19:37:50 +0300

    chore: add cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,14 @@
-import express from "express";
-import routes from "./routes/index.routes";
-require("dotenv").config();
+import express from 'express';
+import routes from './routes/index.routes';
+import cors from 'cors';
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
-app.use("/", routes);
+app.use(cors());
+app.use('/', routes);
 
 app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
